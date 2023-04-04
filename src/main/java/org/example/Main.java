0c12a5e5004744096ea16daa6b98a73355edb7d2
JecuMihaela <mihaela.nicoleta.jecu@gmail.com>
2023-04-04 10:42:02 +0300

    From branch1(local) to branch2?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!From branch1(local) to master");
+        System.out.println("Hello world!From branch1(local) to branch2");
 
 
     }
