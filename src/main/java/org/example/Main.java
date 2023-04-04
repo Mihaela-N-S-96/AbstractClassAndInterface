6f8e447739d7fa1813976628c0ad6d0fec8bd5ad
JecuMihaela <mihaela.nicoleta.jecu@gmail.com>
2023-04-04 11:48:04 +0300

    simple example for revenu calculater using just classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!From branch1(local) to branch1");
 
+        final ClientEngagement clientEngagement =
+                new ClientEngagement("Pluralsight", 100, 15000);
+
+        final double hourlyPrice = RevenueCalculator.price("Hourly", clientEngagement);
+        System.out.println("hourlyPrice = " + hourlyPrice);
+
+        final double fixedFeePrice = RevenueCalculator.price("FixedFee", clientEngagement);
+        System.out.println("FixedPrice = " + fixedFeePrice);
+
+        final double royalPercentage = RevenueCalculator.price("RoyalPercentage", clientEngagement);
+        System.out.println("RoyalPercentage = " + royalPercentage);
 
     }
 }
---
+++ src/main/java/org/example/RevenueCalculator.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class RevenueCalculator {
+
+    private static final double HOURLY_RATE = 50;
+    private static final double FIXED_FEE = 500;
+    private static final double ROYALTY_PERCENTAGE = 0.15;
+
+    public static double price (final String method, final ClientEngagement clientEngagement){
+
+        switch (method)
+        {
+            case "Hourly":
+            return HOURLY_RATE * clientEngagement.getHoursWorked();
+
+            case "FixedFee":
+            return FIXED_FEE;
+
+            case "RoyalPercentage":
+            return ROYALTY_PERCENTAGE;
+
+            default:
+                throw new IllegalArgumentException("Unknown method" + method);
+        }
+
+
+    }
+}
